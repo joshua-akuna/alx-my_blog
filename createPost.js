e201b546a84ef85e30bc37b2ac041116782cbc2e
Akuna Joshua <akunajoshua@gmail.com>
2024-03-17 10:49:32 +0100

    added funtionalities to extract the frontmatter of markdown files

--- createPost.js
+++ createPost.js
@@ -4,8 +4,10 @@ const marked = require("marked")
 const fs = require("fs")
 
 const createPost = path => {
+    // reads the content to the markdown path directory in utf8
     const post = fs.readFileSync(`${config.dev.postsdir}/${path}.md`, "utf8")
-    const content = post
+    // extracts the frontmatter content of the markdown file
+    const content = fm(post)
     console.log(content)
     console.log("POST")
 }
