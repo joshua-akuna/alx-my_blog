7d111ca2612c3b6e161bb5ab71969cabe510ee43
Akuna Joshua <akunajoshua@gmail.com>
2024-03-17 11:04:12 +0100

    implemented the createPost function

--- createPost.js
+++ createPost.js
@@ -1,15 +1,17 @@
 const config = require("./config")
 const fm = require("front-matter")
-const marked = require("marked")
+const { marked } = require("marked")
 const fs = require("fs")
 
 const createPost = path => {
     // reads the content to the markdown path directory in utf8
     const post = fs.readFileSync(`${config.dev.postsdir}/${path}.md`, "utf8")
-    // extracts the frontmatter content of the markdown file
+    // extracts the content of the markdown file as an object
     const content = fm(post)
-    console.log(content)
-    console.log("POST")
+    // converts the markdown content to HTML
+    content.body = marked(content.body)
+    content.path = path
+    return content
 }
 
 module.exports = createPost
