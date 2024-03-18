3bece90c73a7b724e49bc2aca7b7ef4fb710a84a
Akuna Joshua <akunajoshua@gmail.com>
2024-03-18 06:39:54 +0100

    added and configured the styles.css file

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 const port = 3000
 
 app.set('view engine', 'ejs')
+app.use('/public', express.static('public'))
 
 app.get('/', (req, res)=>{
     res.render('index', {posts: posts})
